c8975e9c6646985cc6fb0a708168589bf00c704c
Héctor Adrián <hectorjaz2004@gmail.com>
2026-06-22 20:12:10 -0600

    Arreglo en logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -2,21 +2,27 @@ import winston from "winston";
 import * as appInsights from "applicationinsights";
 import { envs } from "./envs";
 
-appInsights
+let aiClient: appInsights.TelemetryClient | undefined;
+
+if (envs.APPINSIGHTS_CONNECTION_STRING) {
+  appInsights
     .setup(envs.APPINSIGHTS_CONNECTION_STRING)
     .setAutoCollectConsole(false)
     .setSendLiveMetrics(true)
-    .start()
+    .start();
 
-const aiClient = appInsights.defaultClient;
+  aiClient = appInsights.defaultClient;
+}
  
 const appInsightsTransport = new winston.transports.Console({
   level: "info",
   format: winston.format.printf(({ level, message, timestamp }) => {
-    aiClient.trackTrace({
-      message: `[${level}] ${message}`,
-      properties: { timestamp },
-    });
+    if (aiClient) {
+      aiClient.trackTrace({
+        message: `[${level}] ${message}`,
+        properties: { timestamp },
+      });
+    }
     return `${timestamp} [${level}] ${message}`;
   }),
 });
